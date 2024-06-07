56519ed2e82fb8655355212f3ae2cfbb01efadc7
bobobbobobboboboobboobbo <165218201+bobobbobobboboboobboobbo@users.noreply.github.com>
2024-06-07 14:16:10 +0000

    all done for this version

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 file_number = int(input ("""pick your adventure
 1. ride of paul revere
+2. battle of tripoli
                          choice: """)) -1
-file_list = ["f1.text", 0, 0, 0]
+file_list = ["f1.text", "f2.text", 0, 0] #add your own file here to use.
 current_file = file_list [file_number]
 current_file = open(current_file, "r")
 context = current_file.read ()
@@ -15,36 +16,23 @@ while True:
     user_input =''
     while True:
         genai.configure(api_key=key) 
-        #if error==True: 
-            #model = 'gemini-1.0-pro-latest'
-            #error_input = 'do not output anything but a single blank space character.'
-            #k = True
         if error == False: 
             time.sleep (0.1)
+            print ("_________________________________________________")
             user_input = input ("input: ")
             model = 'gemini-1.5-flash-latest'
             k = False
-        #print ("model:", model ,"\n _______________________")
         try:
-            #if k==True: total_input = error_input
             if k==False: total_input = context + "\n" + user_input
             model = genai.GenerativeModel(model)
             response = model.generate_content(total_input)
-            #error = False
-        
+            print ("_____________________________________________________________")
             print("\n\n\n"+response.text )
             if k == False: context = context + "\n" + response.text + "\n"
-            
-            #if k == True: 
-                #context = context
-
-            
-
-            #file_number +=1
             error = False
         except: 
-            #error=True
             print (" \n \n \n \n _________________________ \n AN ERROR HAS OCCURED WARNING WARNING AN ERROR HAS OCCURED \n ____________________________________")
+            print ("this is the context just in case you need to restart. in the event of an infinite error chain restart the game and input this as you first input:  " + context, end='\n')
+            input ("do you understand. once you give an input here the error chain may or may not continue. if it doesn't keep playing, if it does resart and insert the context you copied: ")
             break
-        #error = False
 print (promptnum)
